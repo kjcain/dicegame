eea4e108d79566b4e1d2be2d2070e78130f25d2d
Kyler Cain <kyler.cain@yahoo.com>
2026-01-20 10:40:41 -0500

    report run time and rate

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod dice;
 mod game;
 
 use rayon::prelude::*;
+use std::time::Instant;
 
 fn main() {
     pretty_env_logger::init();
@@ -15,10 +16,17 @@ fn main() {
     log::info!("Using dice: {:?}", game.dice);
     log::info!("Running {} iterations in parallel across all CPU cores", args.iterations);
 
+    let start = Instant::now();
+    
     let wins: u32 = (0..args.iterations)
         .into_par_iter()
         .map(|_| if game.play() { 1 } else { 0 })
         .sum();
 
+    let duration = start.elapsed();
+    let iterations_per_second = args.iterations as f64 / duration.as_secs_f64();
+
+    log::debug!("Total duration: {:.3}s", duration.as_secs_f64());
+    log::debug!("Iterations per second: {:.2}", iterations_per_second);
     log::info!("Win Rate: {:.2}% across {} iterations", (wins as f64 / args.iterations as f64) * 100.0, args.iterations);
 }
